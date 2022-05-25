3e5eae28d3a688df67209e7885172736d9efd3de
carmabol <carlos.mario.borrero@correounivalle.edu.co>
2022-05-25 15:07:08 -0500

    Estructura Angular y HTML divs y lógica dark-light

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,10 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  modeSelected=0;
+  mode=[
+    { 
+      img_switch:'../../assets/images/icon-sun.svg',
 
+    },
+    {
+      img_switch:'../../assets/images/icon-moon.svg',
+    }];
+  
+  items=[
+    {
+      isComplete:true,
+      description:'Complete online JavaScript course'
+    },
+    {
+      isComplete:false,
+      description:'Jog around the park 3x'
+    },
+    {
+      isComplete:false,
+      description:'10 minutes meditation'
+    },
+    {
+      isComplete:false,
+      description:'Read for 1 hour'
+    },
+    {
+      isComplete:false,
+      description:'Pick up groceries'
+    },
+    {
+      isComplete:false,
+      description:'Complete Todo App on Frontend Mentor'
+    },
+
+  ]
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  getItemsLeft(){
+    let count=this.items.length;
+    for (const iterator of this.items) {
+      if(iterator.isComplete)
+        count--;
+    }
+    return count;
   }
 
 }
