6bc466e1b2482eaea843a42052389bfbc0fce417
carmabol <carlos.mario.borrero@correounivalle.edu.co>
2022-05-25 08:48:08 -0500

    Diseño de la App en Estructura Angular Para inicio de CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'todo',
+    component: TodoComponent
+  }
+  ,
+  {
+    path: '**',
+    redirectTo: 'todo',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+    //AppRoutingModule
+  ],
+  exports:[
+    TodoComponent,
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
